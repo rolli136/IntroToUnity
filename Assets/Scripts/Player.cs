e37a03cbbac2510023547dfde40ef90403041dbb
Hanna Algedri <hannaalgedri@Hannas-MBP.fritz.box>
2021-03-31 13:26:21 +0200

    Changed Position of the Camera, it will now follow the Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,7 +16,7 @@ public class Player : MonoBehaviour
     void Start()
     {
        
-        transform.position = new Vector3(0, 0, 0);
+        transform.position = new Vector3(0f, 0f, 0.5f);
     }
 
     // Update is called once per frame
